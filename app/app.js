9d17be261c5e35332d5b3be8d4be734426838ed0
GoodmanNgwenya <lacosterg55@gmail.com>
2020-07-08 05:54:49 +0200

    first commit

---
+++ app/app.js
@@ -0,0 +1,33 @@
+function startGame() {
+    // starting a new game
+    var playerName = getInputValue("playername");
+    logPlayer(playerName);
+    postScore(80, playerName);
+    postScore(-5, playerName);
+}
+function logPlayer(name) {
+    if (name === void 0) { name = 'MultiMath Player'; }
+    console.log("New game starting for player: " + name);
+}
+
+function postScore(score, playerName) {
+    if (playerName === void 0) { playerName = 'MultiMath Player'; }
+    var logger;
+    if (score < 0) {
+        logger = logError;
+    }
+    else {
+        logger = logMessage;
+    }
+    var scoreElement = document.getElementById('postedScores');
+    scoreElement.innerText = score + " - " + playerName;
+    logger("Score: " + score);
+}
+document.getElementById('startGame').addEventListener('click', startGame);
+var logMessage = function (message) { return console.log(message); };
+function logError(err) {
+    console.error(err);
+}
+var firstPlayer = new Player();
+firstPlayer.name = 'Goodman';
+console.log(firstPlayer.formatName());
---
+++ app/register.ts
@@ -0,0 +1,27 @@
+const form: HTMLFormElement = document.querySelector('#myform');
+
+form.onsubmit = () => {
+  const formData = new FormData(form);
+
+  console.log(formData);
+  const firstname = formData.get('forenames') as string;
+ const surname = formData.get('lastname') as string;
+ const nickname = formData.get('nickname') as string;
+ const email = formData.get('email') as string;
+ const password = formData.get('password') as string;
+ const newpassword = formData.get('newpassword') as string;
+ console.log(firstname);
+console.log(surname);
+console.log(nickname);
+ console.log(email);
+  return false; 
+};
+function terms_change(checkbox){
+    if(checkbox.checked){
+      var element = <HTMLInputElement> document.getElementById("btnRegister");
+      element.disabled = false;
+        } else{
+          var element = <HTMLInputElement> document.getElementById("btnRegister");
+          element.disabled = true;
+        }
+}
